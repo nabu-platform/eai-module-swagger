d44fdf9ba2e942437578bbe56625f207fc7bdd8c
Alexander Verbruggen <alex@nabu.be>
2023-08-25 16:03:56 +0200

    updated

--- src/main/java/nabu/web/swagger/Services.java
+++ src/main/java/nabu/web/swagger/Services.java
@@ -1,11 +1,15 @@
 package nabu.web.swagger;
 
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import be.nabu.eai.module.swagger.provider.SwaggerProvider;
 import be.nabu.eai.repository.EAIResourceRepository;
 import be.nabu.libs.artifacts.api.Artifact;
+import be.nabu.libs.swagger.formatter.SwaggerFormatter;
+import be.nabu.libs.types.DefinedTypeResolverFactory;
+import be.nabu.libs.types.api.DefinedType;
 
 @WebService
 public class Services {
@@ -20,6 +24,15 @@ public class Services {
 				throw new IllegalArgumentException("Not a swagger provider: " + swaggerProviderId);
 			}
 		}
+	}
+	
+	@WebResult(name = "json")
+	public String formatAsJsonSchema(@WebParam(name = "typeId") String typeId) {
+		DefinedType resolved = DefinedTypeResolverFactory.getInstance().getResolver().resolve(typeId);
+		if (resolved == null) {
+			throw new IllegalArgumentException("Not a type: " + typeId);
+		}
+		return SwaggerFormatter.formatTypeAsJSON(resolved);	
 	}
 	
 }
